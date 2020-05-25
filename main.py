0c0c12906dcb9cccdff672c5d7f416ec0f9f78de
Chris Tsatsis <32398056+chrisgt21@users.noreply.github.com>
2020-05-25 17:30:08 -0400

    removed variables from main

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
-
+JWT_SECRET = os.environ.get('JWT_SECRET')
+LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
 def _logger():
     '''
